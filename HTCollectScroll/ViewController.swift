d53f7a41136e9635335db044d11353bc58fe0dff
haitaowu <taotaowoo@gmail.com>
2016-01-16 14:21:51 +0800

    Initial Commit

---
+++ HTCollectScroll/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HTCollectScroll
+//
+//  Created by taotao on 1/16/16.
+//  Copyright © 2016 taotao. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
